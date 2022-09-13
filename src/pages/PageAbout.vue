df88bf48c7a25430f5ef7824bb5f596fd68b0bdf
lapinRagnar <lapinragnar@gmail.com>
2022-09-13 07:25:11 +0200

    6 - about page

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -1,7 +1,18 @@
 <template>
-  <q-page padding>
-    About page
+
+  <q-page padding class="q-pa-lg">
+
+    <h4 class="q-mt-none q-mb-md text-weight-bold">A props du qwitter</h4>
+    <div class="text-body1">
+
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis saepe, dolore a dolores recusandae voluptatibus asperiores ex vitae. Praesentium veritatis obcaecati, deleniti exercitationem dicta repellendus voluptate explicabo fuga aut hic!</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis saepe, dolore a dolores recusandae voluptatibus asperiores ex vitae. Praesentium veritatis obcaecati, deleniti exercitationem dicta repellendus voluptate explicabo fuga aut hic!</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis saepe, dolore a dolores recusandae voluptatibus asperiores ex vitae. Praesentium veritatis obcaecati, deleniti exercitationem dicta repellendus voluptate explicabo fuga aut hic!</p>
+
+
+    </div>
   </q-page>
+
 </template>
 
 <script>
