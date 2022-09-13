c498093f6c59e5fd32a094e970d66aea207b97b0
lapinRagnar <lapinragnar@gmail.com>
2022-09-13 07:46:25 +0200

    agrandir la taille de l'avatar

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -14,19 +14,12 @@
       >
 
       <template v-slot:before>
-        <q-avatar>
+        <q-avatar size="xl">
           <img src="https://cdn.quasar.dev/img/avatar5.jpg">
         </q-avatar>
       </template>
 
-      <template v-slot:append>
-        <q-icon v-if="text !== ''" name="close" @click="text = ''" class="cursor-pointer" />
-        <q-icon name="schedule" />
-      </template>
 
-      <template v-slot:hint>
-        Field hint
-      </template>
 
       <template v-slot:after>
         <q-btn round dense flat icon="send" />
